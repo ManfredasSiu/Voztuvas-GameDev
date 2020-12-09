39c14edddf32aebb37cfc0d74fb5ef19d9317246
Tristanas <43069390+Tristanas@users.noreply.github.com>
2020-12-09 19:18:22 +0200

    Structural changes, level finish logic
    Added a signpost to mark the end of a level.

---
+++ Voztuvas/Assets/Scripts/LevelFinishController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelFinishController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Play some animation, sounds and load next level.
+        int sceneCount = SceneManager.sceneCount;
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        if (currentScene < sceneCount)
+        {
+            SceneManager.LoadScene(currentScene + 1);
+        }
+        else
+        {
+            Debug.LogWarning("Trying to load a non-existing scene. (LevelFinishController)");
+        }
+        
+    }
+}
