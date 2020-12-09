3d845d07995f4a5b842902d8d11ab597cc8a0528
Tristanas <43069390+Tristanas@users.noreply.github.com>
2020-12-09 15:33:21 +0200

    Saved prefabs, added guide text and some to-dos

--- Voztuvas/Assets/Scripts/Trap/TriggerDamage.cs
+++ Voztuvas/Assets/Scripts/Trap/TriggerDamage.cs
@@ -6,11 +6,23 @@ using UnityEngine;
 public class TriggerDamage : MonoBehaviour
 {
     public float damage;
+    private Rigidbody2D _body;
 
+    void Start()
+    {
+        _body = GetComponentInParent<Rigidbody2D>();
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Spikes entered the players body!");
+
+        if (_body.velocity.magnitude == 0) // The trap has already fallen down and can't deal damage.
+        {
+            Destroy(this);
+            return;
+        }
+
+        Debug.Log("Spikes entered the entities body!");
         var health = other.gameObject.GetComponent<HealthController>();
         if(health != null && other.gameObject.tag == "Player" || other.gameObject.tag == "Mob")
         {
