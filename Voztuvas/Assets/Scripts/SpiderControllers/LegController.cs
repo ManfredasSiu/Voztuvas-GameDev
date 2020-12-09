9cd611901066fd2ccb77c2f41566ed46a4c60317
dariu <dariuskrtn@gmail.com>
2020-12-10 00:14:31 +0200

    spider mov

---
+++ Voztuvas/Assets/Scripts/SpiderControllers/LegController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.SpiderControllers
+{
+    public class LegController : MonoBehaviour
+    {
+        public bool CanStep = true;
+        public bool WouldLikeToStep = false;
+        private float StepSize = 2f;
+        public LayerMask GroundMask;
+        public Transform Leg;
+        public Transform Body;
+        public Action OnStepStarted;
+        public Action OnStepEnded;
+
+        private bool _isStepping = false;
+
+
+        void Start()
+        {
+            transform.position = Leg.position;
+        }
+
+        void Update()
+        {
+            if (_isStepping || !CanStep) return;
+
+            WouldLikeToStep = false;
+
+            TryMoveLeg();
+        }
+
+        Vector2? GetTargetPoint()
+        {
+            var rotation = Body.TransformDirection(Vector3.down);
+            var velocity = ToVector3(Body.GetComponent<Rigidbody2D>().velocity)*0.35f;
+
+            var forDebug = new Vector3(rotation.x, rotation.y, rotation.z) + velocity;
+            forDebug.Scale(new Vector3(30, 30, 30));
+            Debug.DrawRay(transform.position + Reversed(rotation), forDebug, Color.red);
+
+            var raycast = Physics2D
+                .Raycast(transform.position + Reversed(rotation), rotation + velocity, 4f, GroundMask);
+
+            return raycast.transform == null ? (Vector2?)null : raycast.point;
+        }
+
+        public void TryMoveLeg()
+        {
+            var target = GetTargetPoint();
+
+            if (target != null)
+                Debug.DrawLine(ToVector3(target.Value), ToVector3(target.Value) + Vector3.up, Color.red);
+
+            if (target != null && Vector2.Distance(Leg.position, target.Value) >= StepSize)
+                MoveLeg(target.Value);
+        }
+        public void MoveLeg(Vector2 target)
+        {
+            if (CanStep)
+            {
+                OnStepStarted?.Invoke();
+                StartCoroutine(MoveLegCoroutine(target));
+            }
+            else
+                WouldLikeToStep = true;
+        }
+
+        IEnumerator MoveLegCoroutine(Vector2 target)
+        {
+            if (!CanStep)
+                yield break;
+
+            _isStepping = true;
+            var stepTarget = target;
+
+            Leg.position = Vector3.Lerp(Leg.position, Body.position, Time.deltaTime * 7);
+            yield return null;
+
+            while (Vector3.Distance(stepTarget, Leg.position) > 0.1f)
+            {
+                Leg.position = Vector3.Lerp(Leg.position, stepTarget, Time.deltaTime * 7);
+                yield return null;
+            }
+
+            Leg.position = stepTarget;
+
+            _isStepping = false;
+            OnStepEnded?.Invoke();
+        }
+
+        Vector3 Reversed(Vector3 vector)
+        {
+            return new Vector3(-1.5f * vector.x, -1.5f * vector.y, -1.5f*vector.z);
+        }
+
+        Vector3 ToVector3(Vector2 vector)
+        {
+            return new Vector3(vector.x, vector.y, 0);
+        }
+    }
+}
---
+++ Voztuvas/Assets/Scripts/SpiderControllers/MovementController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Assets.Scripts.SpiderControllers
+{
+    public class MovementController : MonoBehaviour
+    {
+        public LayerMask GroundMask;
+        public float RunSpeed = 10f;
+        public float RotationDelta = 1;
+        public List<GameObject> LeftLegs = new List<GameObject>();
+        public List<GameObject> RightLegs = new List<GameObject>();
+
+        public List<LegController> LeftLegsControllers = new List<LegController>();
+        public List<LegController> RightLegsControllers = new List<LegController>();
+
+        private Rigidbody2D _rb;
+        private float _xAxis;
+        private float _yAxis;
+        private bool _canMove = true;
+
+        void Start()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+
+            DisableSameSideLegsOnStep(LeftLegsControllers);
+            DisableSameSideLegsOnStep(RightLegsControllers);
+        }
+
+        void Update()
+        {
+            _xAxis = Input.GetAxis("Horizontal");
+            _yAxis = Input.GetAxis("Vertical");
+
+            if (Input.GetKey(KeyCode.Space))
+                StartCoroutine(DropFromCeiling());
+
+            if (_canMove)
+            {
+                Run(new Vector2(_xAxis, _yAxis));
+
+                var optimalRotation = ComputeOptimalRotation();
+
+                if (Math.Abs(transform.rotation.z - optimalRotation) >= RotationDelta)
+                {
+                    RotateBody(optimalRotation);
+                }
+            }
+        }
+
+        private IEnumerator DropFromCeiling()
+        {
+            _canMove = false;
+            var firstLegs = Random.Range(0, 1) * 2;
+
+            var legs = LeftLegs.Concat(RightLegs).ToList();
+            var controllers = LeftLegsControllers.Concat(RightLegsControllers).ToList();
+
+            controllers.ToList()
+                .ForEach(leg => leg.CanStep = false);
+
+            legs.Take(2).ToList()
+                .ForEach(leg => leg.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic);
+
+            yield return new WaitForSeconds(0.3f);
+            _rb.gravityScale = 1;
+
+
+
+            transform.rotation = new Quaternion();
+
+            legs.Skip(firstLegs).Take(2).ToList()
+                .ForEach(leg => leg.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic);
+        }
+
+        private void StandUp()
+        {
+            _canMove = true;
+            _rb.gravityScale = 0;
+
+            LeftLegsControllers.ForEach(leg => leg.CanStep = true);
+            RightLegsControllers.ForEach(leg => leg.CanStep = true);
+            LeftLegs.ForEach(leg => leg.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static);
+            RightLegs.ForEach(leg => leg.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static);
+        }
+
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            Debug.Log(collision.collider);
+            if (!_canMove && _rb.IsTouchingLayers(GroundMask.value))
+            {
+                StandUp();
+            }
+        }
+
+        private void Run(Vector2 dir)
+        {
+            _rb.velocity = new Vector2(dir.x * RunSpeed, dir.y * RunSpeed);
+        }
+
+        void DisableSameSideLegsOnStep(List<LegController> legs)
+        {
+            foreach (var leg in legs)
+            {
+                leg.OnStepStarted = () =>
+                {
+                    var legC = legs.Single(l => l != leg).GetComponent<LegController>();
+                    legC.CanStep = false;
+                };
+
+                leg.OnStepEnded = () =>
+                {
+                    var legC = legs.Single(l => l != leg).GetComponent<LegController>();
+                    legC.CanStep = true;
+                    if (legC.WouldLikeToStep)
+                        legC.TryMoveLeg();
+                };
+            }
+        }
+
+        private void RotateBody(float rotation)
+        {
+            transform.eulerAngles = new Vector3(0, 0, rotation);
+        }
+
+        private float ComputeOptimalRotation()
+        {
+            var leftLegs = LegsSum(LeftLegs);
+            var rightLegs = LegsSum(RightLegs);
+
+            if (leftLegs.x > rightLegs.x)
+            {
+                return 180f - Quaternion.LookRotation((leftLegs - rightLegs).normalized).eulerAngles.x;
+            } 
+
+            return Quaternion.LookRotation((leftLegs - rightLegs).normalized).eulerAngles.x;
+        }
+
+        private Vector3 LegsSum(IEnumerable<GameObject> legs) =>
+            legs
+                .Select(leg => leg.transform.position)
+                .Aggregate((current, point) => current + point);
+    }
+}
